6989eaf8062b2a58ef9d3420c1ecbc2b662845cd
hakan çakmak <hakan.cakmak@live.com>
2021-03-04 13:48:14 +0300

    programın backend tarafı tamamlandı. checkboxlar iyi bir yöntemle olmasa da tek seçimlik hale getirildi.Ayrıca buton kaldırıldı.

--- app/src/main/java/com/badlogic/androidgames/gezegenos/MainActivity.kt
+++ app/src/main/java/com/badlogic/androidgames/gezegenos/MainActivity.kt
@@ -9,11 +9,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     // 6- View.OnCLickListener implemente ediyoruz. tıklanma olayları için.
-    //  1- Sabitlerimizi ekleyelim
+    // 1- Sabitlerimizi ekleyelim
     val KgToPound = 2.2046226218
     val PoundToKg = 0.45359237
-
-    var userKg : String = ""
 
     val Mercury = 0.38
     val Venus = 0.91
@@ -29,23 +27,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 2- Değişkenlerimizi tanımlayalım
-        //val userKg = findViewById<TextView>(R.id.ptInput)
-        //val btnHesapla = findViewById<Button>(R.id.btnHesapla)
-        //val etSonuc = findViewById<TextView>(R.id.tvSonuc)
-        userKg = ptInput.text.toString()
+        //- dinleyicileri ekleyelim
+        cBmercury.setOnClickListener(this)
+        cBvenus2.setOnClickListener(this)
+        cBearth2.setOnClickListener(this)
+        cBmars.setOnClickListener(this)
+        cBjupiter2.setOnClickListener(this)
+        cBsaturn.setOnClickListener(this)
+        cBuranus.setOnClickListener(this)
+        cBneptun.setOnClickListener(this)
+        cBpluto.setOnClickListener(this)
 
+        // 2- Değişkenlerimizi tanımlayalım //9- başl. eklediğimiz değişkenleri kaldırdık. null değer dönmesine sebp olacak çünkü. yenisini ekliyoruz.
+        //val etSonuc = findViewById<TextView>(R.id.tvSonuc) && userKg = ptInput.text.toString()
 
-
-        // 4- Butona işlev kazandıralım
-        btnHesapla.setOnClickListener {
-            var userPound = kiloToPound(userKg.toString().toDouble())
-            var mercuryPound = userPound * Mercury
-            var mercuryKg = poundToKilo(mercuryPound)
-
-            // 6- formatter metodunu ekleyelim
-            tvSonuc.text = mercuryKg.formatter(2).toString()
-        }
+        // 4- Butona işlev kazandıralım //btn kaldırıldı
     }
 
     // 3 - Dönüştürücü func. ı50mızı yazalım
@@ -60,38 +56,124 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     // 5- sonuç sayının virgülden sonraki göstermek istediğimiz basamak mikt. belirliyoruz
     fun Double.formatter(numberCount:Int) = java.lang.String.format("%.${numberCount}f", this)
 
-    // 7- İmp. metoumuzu kodlayalım
-    override fun onClick(v: View?) {
+    // 7- İmp. metoumuzu kodlayalım 10-// pt boş iken hata versin istemiyoruz
+        override fun onClick(v: View?) {
         v as CheckBox
         var isChecked:Boolean = v.isChecked
 
-        when(v.id) {
-            R.id.cBmercury -> if(isChecked && !TextUtils.isEmpty(ptInput.toString())) {
+        if (!TextUtils.isEmpty(ptInput.text.toString()))
+        {
+            var userKg = ptInput.text.toString().toDouble()
+            var userPound = kiloToPound(userKg)
 
-            }
-            R.id.cBvenus2 -> if(isChecked && !TextUtils.isEmpty(ptInput.toString())) {
+            when(v.id) {
+                R.id.cBmercury -> if(isChecked) {
+                    hesaplaPound(userPound, v)
+                    cBvenus2.isChecked = false;
+                    cBmars.isChecked = false;
+                    cBearth2.isChecked = false;
+                    cBjupiter2.isChecked = false;
+                    cBsaturn.isChecked = false;
+                    cBneptun.isChecked = false;
+                    cBuranus.isChecked = false;
+                    cBpluto.isChecked = false;
 
-            }
-            R.id.cBearth2 -> if(isChecked && !TextUtils.isEmpty(ptInput.toString())) {
+                }
+                R.id.cBvenus2 -> if(isChecked) {
+                    hesaplaPound(userPound, v)
+                    cBmercury.isChecked = false;
+                    cBmars.isChecked = false;
+                    cBearth2.isChecked = false;
+                    cBjupiter2.isChecked = false;
+                    cBsaturn.isChecked = false;
+                    cBneptun.isChecked = false;
+                    cBuranus.isChecked = false;
+                    cBpluto.isChecked = false;
 
-            }
-            R.id.cBmars -> if(isChecked && !TextUtils.isEmpty(ptInput.toString())) {
+                }
+                R.id.cBearth2 -> if(isChecked) {
+                    hesaplaPound(userPound, v)
+                    cBvenus2.isChecked = false;
+                    cBmars.isChecked = false;
+                    cBmercury.isChecked = false;
+                    cBjupiter2.isChecked = false;
+                    cBsaturn.isChecked = false;
+                    cBneptun.isChecked = false;
+                    cBuranus.isChecked = false;
+                    cBpluto.isChecked = false;
 
-            }
-            R.id.cBjupiter2-> if(isChecked && !TextUtils.isEmpty(ptInput.toString())) {
+                }
+                R.id.cBmars -> if(isChecked) {
+                    hesaplaPound(userPound, v)
+                    cBvenus2.isChecked = false;
+                    cBmercury.isChecked = false;
+                    cBearth2.isChecked = false;
+                    cBjupiter2.isChecked = false;
+                    cBsaturn.isChecked = false;
+                    cBneptun.isChecked = false;
+                    cBuranus.isChecked = false;
+                    cBpluto.isChecked = false;
 
-            }
-            R.id.cBsaturn -> if(isChecked && !TextUtils.isEmpty(ptInput.toString())) {
+                }
+                R.id.cBjupiter2-> if(isChecked) {
+                    hesaplaPound(userPound, v)
+                    cBvenus2.isChecked = false;
+                    cBmars.isChecked = false;
+                    cBearth2.isChecked = false;
+                    cBmercury.isChecked = false;
+                    cBsaturn.isChecked = false;
+                    cBneptun.isChecked = false;
+                    cBuranus.isChecked = false;
+                    cBpluto.isChecked = false;
 
-            }
-            R.id.cBuranus -> if(isChecked && !TextUtils.isEmpty(ptInput.toString())) {
+                }
+                R.id.cBsaturn -> if(isChecked) {
+                    hesaplaPound(userPound, v)
+                    cBvenus2.isChecked = false;
+                    cBmars.isChecked = false;
+                    cBearth2.isChecked = false;
+                    cBjupiter2.isChecked = false;
+                    cBmercury.isChecked = false;
+                    cBneptun.isChecked = false;
+                    cBuranus.isChecked = false;
+                    cBpluto.isChecked = false;
 
-            }
-            R.id.cBneptun -> if(isChecked && !TextUtils.isEmpty(ptInput.toString())) {
+                }
+                R.id.cBuranus -> if(isChecked) {
+                    hesaplaPound(userPound, v)
+                    cBvenus2.isChecked = false;
+                    cBmars.isChecked = false;
+                    cBearth2.isChecked = false;
+                    cBjupiter2.isChecked = false;
+                    cBsaturn.isChecked = false;
+                    cBneptun.isChecked = false;
+                    cBmercury.isChecked = false;
+                    cBpluto.isChecked = false;
 
-            }
-            R.id.cBpluto -> if(isChecked && !TextUtils.isEmpty(ptInput.toString())) {
+                }
+                R.id.cBneptun -> if(isChecked) {
+                    hesaplaPound(userPound, v)
+                    cBvenus2.isChecked = false;
+                    cBmars.isChecked = false;
+                    cBearth2.isChecked = false;
+                    cBjupiter2.isChecked = false;
+                    cBsaturn.isChecked = false;
+                    cBmercury.isChecked = false;
+                    cBuranus.isChecked = false;
+                    cBpluto.isChecked = false;
 
+                }
+                R.id.cBpluto -> if(isChecked) {
+                    hesaplaPound(userPound, v)
+                    cBvenus2.isChecked = false;
+                    cBmars.isChecked = false;
+                    cBearth2.isChecked = false;
+                    cBjupiter2.isChecked = false;
+                    cBsaturn.isChecked = false;
+                    cBneptun.isChecked = false;
+                    cBuranus.isChecked = false;
+                    cBmercury.isChecked = false;
+                }
             }
         }
     }
@@ -114,7 +196,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         var sonucToKg = poundToKilo(sonuc)
-        tvSonuc.text = sonucToKg.toString()
+        tvSonuc.text = sonucToKg.formatter(2)
 
     }
 }
