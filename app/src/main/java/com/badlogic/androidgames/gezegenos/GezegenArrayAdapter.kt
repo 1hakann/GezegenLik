b8ea7ba98949733eb1e2115e82a7696e813178f9
hakan çakmak <hakan.cakmak@live.com>
2021-04-04 00:44:13 +0300

    PlanetArrayAdapter has been configured. The listing of static data has been completed. The code has been optimized.

--- app/src/main/java/com/badlogic/androidgames/gezegenos/GezegenArrayAdapter.kt
+++ app/src/main/java/com/badlogic/androidgames/gezegenos/GezegenArrayAdapter.kt
@@ -1,10 +1,33 @@
 package com.badlogic.androidgames.gezegenos
 
 import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import java.util.*
+import kotlinx.android.synthetic.main.tek_satir.view.*
 
-class GezegenArrayAdapter(context: Context, resource: Int, textViewResourceId: Int, Gezegenler:Array<String>, GezAciklama:Array<String>, GezRes:Array<Int>)
-    : ArrayAdapter<String>(context, resource, textViewResourceId, Gezegenler) {
+class GezegenArrayAdapter(var gelenContext: Context, resource: Int, textViewResourceId: Int, var Gezegenler:Array<String>, var GezAciklama:Array<String>, var GezRes:Array<Int>)
+    : ArrayAdapter<String>(gelenContext, resource, textViewResourceId, Gezegenler) {
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tek_satir_view = convertView
+        if (tek_satir_view == null) {
+            val myInflater = LayoutInflater.from(gelenContext)
+            tek_satir_view = myInflater.inflate(R.layout.tek_satir, parent, false)
+            Log.e("HAKAN","Inflation Yapıldı ${Gezegenler[position]}")
+        }
+
+        var gezegenAdi = tek_satir_view?.tvGezAdi
+        var gezAciklama = tek_satir_view?.tvGezAcikl
+        var gezResim = tek_satir_view?.ivGezegen
+
+        gezegenAdi?.setText(Gezegenler[position])
+        gezAciklama?.setText(GezAciklama[position])
+        gezResim?.setImageResource(GezRes[position])
+
+        return tek_satir_view!!
+    }
 }
