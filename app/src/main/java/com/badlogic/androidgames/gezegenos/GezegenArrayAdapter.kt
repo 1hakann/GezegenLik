77edb4fa512a05df7c314c1c47209bdd6dcc55c2
hakan çakmak <hakan.cakmak@live.com>
2021-04-05 02:28:06 +0300

    optimization completed.

--- app/src/main/java/com/badlogic/androidgames/gezegenos/GezegenArrayAdapter.kt
+++ app/src/main/java/com/badlogic/androidgames/gezegenos/GezegenArrayAdapter.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.tek_satir.view.*
 
 class GezegenArrayAdapter(var gelenContext: Context, resource: Int, textViewResourceId: Int, var Gezegenler:Array<String>, var GezAciklama:Array<String>, var GezRes:Array<Int>)
@@ -14,20 +17,36 @@ class GezegenArrayAdapter(var gelenContext: Context, resource: Int, textViewReso
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
         var tek_satir_view = convertView
+        var viewHolder:ViewHolder? = null
         if (tek_satir_view == null) {
             val myInflater = LayoutInflater.from(gelenContext)
             tek_satir_view = myInflater.inflate(R.layout.tek_satir, parent, false)
+            viewHolder = ViewHolder(tek_satir_view)
+            tek_satir_view.tag = viewHolder
             Log.e("HAKAN","Inflation Yapıldı ${Gezegenler[position]}")
+        } else {
+           // viewHolder = ViewHolder(tek_satir_view) yerine
+            viewHolder = tek_satir_view.getTag() as ViewHolder
+
         }
 
-        var gezegenAdi = tek_satir_view?.tvGezAdi
-        var gezAciklama = tek_satir_view?.tvGezAcikl
-        var gezResim = tek_satir_view?.ivGezegen
-
-        gezegenAdi?.setText(Gezegenler[position])
-        gezAciklama?.setText(GezAciklama[position])
-        gezResim?.setImageResource(GezRes[position])
+        viewHolder.gezegenAdi.setText(Gezegenler[position])
+        viewHolder.gezAciklama.setText(GezAciklama[position])
+        viewHolder.gezResim.setImageResource(GezRes[position])
 
         return tek_satir_view!!
+    }
+
+    inner class ViewHolder(tek_satir_view:View) {
+        var gezegenAdi : TextView
+        var gezAciklama : TextView
+        var gezResim : ImageView
+
+        init {
+            this.gezegenAdi = tek_satir_view.tvGezAdi
+            this.gezAciklama = tek_satir_view.tvGezAcikl
+            this.gezResim = tek_satir_view.ivGezegen
+        }
+
     }
 }
