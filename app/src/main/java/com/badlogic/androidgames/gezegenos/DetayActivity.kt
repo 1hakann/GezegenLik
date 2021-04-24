031435c8f78efb5cbece017c7470c22386d67c22
hakan çakmak <hakan.cakmak@live.com>
2021-04-25 00:19:26 +0300

    serialization has been applied. Arraylist posts to detailActivity have been made available. Data is sent to textview with putExtra. android design support library was implemented. DetailActivity design has been redone using CoordinatorLayout.

--- app/src/main/java/com/badlogic/androidgames/gezegenos/DetayActivity.kt
+++ app/src/main/java/com/badlogic/androidgames/gezegenos/DetayActivity.kt
@@ -3,13 +3,18 @@ package com.badlogic.androidgames.gezegenos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 
 class DetayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detay)
 
-        val myText : TextView = findViewById(R.id.tvDetayAdi)
-        myText.text = intent.extras?.get("Gönder").toString()
+       // val myText : TextView = findViewById(R.id.tvDetayAciklama)
+        val gezegenBilgi = intent.extras?.get("GezegenBilgi") as ArrayList<Gezegen>
+       // myText.text = intent.extras?.get("GezegenBilgi").toString()
+
+
+        //Toast.makeText(this, "gezegen sayısı: "+gezegenBilgi.toString(), Toast.LENGTH_LONG).show()
     }
 }
