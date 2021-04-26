7c172b230ac367732bdd347af33445f01588b42c
hakan çakmak <hakan.cakmak@live.com>
2021-04-27 00:10:12 +0300

    Collapsing Toolbar and Appbar Layout are included. Nested ScrollView was used. The contents were made available to the DetailActivity.

--- app/src/main/java/com/badlogic/androidgames/gezegenos/DetayActivity.kt
+++ app/src/main/java/com/badlogic/androidgames/gezegenos/DetayActivity.kt
@@ -4,15 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_detay.*
 
 class DetayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detay)
 
-       // val myText : TextView = findViewById(R.id.tvDetayAciklama)
+        val myText : TextView = findViewById(R.id.tvGezOzellikleri)
+        val position = intent.extras?.get("Gönder") as Int
         val gezegenBilgi = intent.extras?.get("GezegenBilgi") as ArrayList<Gezegen>
-       // myText.text = intent.extras?.get("GezegenBilgi").toString()
+        myText.setText(gezegenBilgi.get(position).GezGenelOzell)
 
 
         //Toast.makeText(this, "gezegen sayısı: "+gezegenBilgi.toString(), Toast.LENGTH_LONG).show()
